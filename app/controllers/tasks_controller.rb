c5d9975df83e03a836861ad82a78841cba8dceb0
Mario Taracena <macrot_zip@hotmail.com>
2012-05-08 19:52:57 -0600

    traying to add aplication layout

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
+      format.js
       format.json { render json: @task }
     end
   end
